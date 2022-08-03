1272b23f3499aa3de242ad49c0905d846ca3cb1c
Aslan Tyulegenov <tyulegenov1999@mail.ru>
2022-08-03 23:29:16 +0600

    IdentityServer4 UI and Certificate

--- Authorization.IdentityServer/Program.cs
+++ Authorization.IdentityServer/Program.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
-using Authorization.IdentityServer;
+using System.Security.Cryptography.X509Certificates;
 using Authorization.IdentityServer.Data;
 using Authorization.IdentityServer.Services;
 using Microsoft.AspNetCore.Identity;
@@ -9,6 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 var assemblyName = typeof(Program).GetTypeInfo().Assembly.GetName().Name;
+
+var filePath = Path.Combine(builder.Environment.ContentRootPath, "IdentityServer4_certificate.pfx");
+var certificate = new X509Certificate2(filePath, "Aslan123");
 
 services.AddDbContext<ApplicationDbContext>(config => {
     config.UseNpgsql(connectionString);
@@ -22,8 +25,8 @@ services.AddDbContext<ApplicationDbContext>(config => {
 
 services.ConfigureApplicationCookie(configuration => {
     configuration.Cookie.Name = "IdentityServer.Cookies";
-    configuration.LoginPath = "/Auth/Login";
-    configuration.LogoutPath = "/Auth/Logout";
+    // configuration.LoginPath = "/Auth/Login";
+    // configuration.LogoutPath = "/Auth/Logout";
 });
 
 services.AddIdentityServer()
@@ -41,13 +44,17 @@ services.AddIdentityServer()
     // .AddInMemoryApiScopes(IdentityServerConfigurations.GetApiScopes())
     // .AddInMemoryClients(IdentityServerConfigurations.GetClients())    
     .AddProfileService<ProfileService>()
-    .AddDeveloperSigningCredential();
+    .AddSigningCredential(certificate);
+    //.AddDeveloperSigningCredential();
 
 services.AddControllersWithViews()
     .AddRazorRuntimeCompilation();
 
 var app = builder.Build();
+app.UseStaticFiles();
 app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseIdentityServer();
 app.UseEndpoints(config => config.MapDefaultControllerRoute());
 
